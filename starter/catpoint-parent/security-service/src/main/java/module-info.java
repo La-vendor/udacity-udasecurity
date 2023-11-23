cf511d52d3a5fcf404489f715ebf6f32d834e4da
Lavendor <mikolaj.jurecki@gmail.com>
2023-11-23 13:31:38 +0100

    Split into modules

---
+++ starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module security.service {
+    requires java.desktop;
+    requires image.service;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+    requires com.miglayout.swing;
+
+
+}
